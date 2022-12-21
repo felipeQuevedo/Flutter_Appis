6ebb167f3588d553a807676f4364029e447c9579
Felipe <felipequevedo3q@gmail.com>
2022-12-20 20:07:37 -0500

    Galeria4

---
+++ galeria_v1/lib/contenido/my_app.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+
+void main (){
+  debugPaintSizeEnabled = false;
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Center(child: _buildImageColumn(),),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 10,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(icon: const Icon(Icons.menu), onPressed: () {},),
+              IconButton(icon: const Icon(Icons.search), onPressed:(){},),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _buildImageColumn(){
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.black26
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildImageRow(1),
+            _buildImageRow(3),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _buildDecoratedImage(int imageIndex)=> Expanded(
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 10, color: Colors.black38),
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      ),
+      margin: const EdgeInsets.all(4),
+      child: Image.asset('images/pic$imageIndex.jpg'),
+    ),
+  );
+  Widget _buildImageRow(int imageIndex)=>Row(
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+  );
+
+}
---
+++ galeria_v1/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import '/contenido/my_app.dart';
+
+void main (){
+  debugPaintSizeEnabled = false;
+  runApp(const MyApp());
+}
+
